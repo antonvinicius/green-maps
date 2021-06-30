320d9290b1d63573b65109a92162f8e6d20e2215
Antônio Vinícius <antoniovrdacosta@gmail.com>
2021-06-30 19:31:25 -0300

    ranking criado

---
+++ Controllers/RankController.cs
@@ -0,0 +1,29 @@
+using System.Linq;
+using System.Threading.Tasks;
+using GreenMaps.Areas.Identity.Data;
+using GreenMaps.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GreenMaps.Controllers
+{
+    public class RankController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<Usuario> _userManager;
+
+        public RankController([FromServices] ApplicationDbContext context, UserManager<Usuario> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Recupera todos os usuários
+            var usuarios = await _context.Users.Include(x => x.PontoColeta).ToListAsync();
+            return View(usuarios);
+        }
+    }
+}
